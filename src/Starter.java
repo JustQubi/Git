debd2a04400369601ceb2e1233102cc31c4bf52b
Justyna Kubinska <just.kubinska@gmail.com>
2018-11-12 11:59:47 +0100

    siema

--- src/Starter.java
+++ src/Starter.java
@@ -1,6 +1,9 @@
 public class Starter {
     public static void main(String[] args) {
+        int age = 5;
+        System.out.println("siema");
+        System.out.println("gjs");
         System.out.println("Czesc tu Just");
-        
+
     }
 }
