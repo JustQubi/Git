2f2971328a9d0b4d438cc728f354b0865eeda012
Christian Lembrandt <christianlembrandt@gmail.com>
2018-11-12 12:29:36 +0100

    zmiana Christiana

--- src/Starter.java
+++ src/Starter.java
@@ -1,5 +1,21 @@
 public class Starter {
     public static void main(String[] args) {
-        System.out.println("Welcome to GIT!!");
+
+        int age = 5;
+        int age2 = 7;
+
+
+        if (age < age2){
+            System.out.println("age jest mniejsze od age 2");
+    } else if (age2 == age) {
+            System.out.println("age2 jest równe age");
+        }else {
+            System.out.println("age2 jest wieksze od age");
+        }
+
+
+
+
     }
+
 }
